58b5f615bd4a48fe03ad1601d83e169f009c7217
ceccs18c25 <60174030+ceccs18c25@users.noreply.github.com>
2020-03-21 17:41:50 +0530

    t8 (ii) completed

---
+++ 2103/prime.pl
@@ -0,0 +1,21 @@
+use 5.010;
+use warnings;
+use strict;
+my $n;
+print "Enter a number:";
+chomp($n=<STDIN>);
+my ($i,$status)=(2,0);
+
+while( $i <= $n-1){
+       if($n%$i == 0){
+               $status = 1;
+               last;
+               }
+               $i++;
+      }
+if($status == 0){
+   print " $n is a prime number\n\n";
+}
+else{
+   print " $n is not a prime number\n\n";
+}
